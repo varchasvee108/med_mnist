c92a400360ca500999e44ef6e269dc4cfd1fb487
varchasvi <varchasvee108@gmail.com>
2026-04-01 12:06:09 +0530

    dataset code update

---
+++ data/dataset.py
@@ -0,0 +1,13 @@
+from medmnist import PathMNIST
+from torchvision import transforms
+
+
+def get_transforms():
+    return transforms.Compose([transforms.ToTensor()])
+
+
+def get_dataset(split: str, data_dir: str):
+    dataset = PathMNIST(
+        split=split, root=data_dir, transform=get_transforms, download=True
+    )
+    return dataset
